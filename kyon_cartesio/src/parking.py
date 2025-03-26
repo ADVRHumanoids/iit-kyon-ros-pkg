ef8f692e1cb2ac640512877d9a42674d3e46afb3
Arturo Laurenzi <arturo.laurenzi@iit.it>
2025-03-26 12:46:51 +0000

    parking

---
+++ kyon_cartesio/src/parking.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python3
+
+import time
+import numpy as np
+import argparse
+import os
+
+import rospy
+
+os.environ['XBOT_VERBOSE'] = '2'
+from xbot_interface import xbot_interface as xb
+from cartesian_interface.pyci_all import *
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--action', choices=('park', 'unpark'))
+args = parser.parse_args()
+
+
+class Parking:
+
+    ikpb = """
+
+solver_options:
+    regularization: 1e-2
+    back_end: osqp
+    
+stack:
+    - [postural]
+
+constraints: [vlim, contact_1, contact_2, contact_3, contact_4]
+
+vlim:
+    type: VelocityLimits
+
+contact_1:
+    type: Cartesian
+    distal_link: contact_1
+    base_link: world
+    indices: [0, 1, 2]
+    
+contact_2:
+    type: Cartesian
+    distal_link: contact_2
+    base_link: world
+    indices: [0, 1, 2]
+    
+contact_3:
+    type: Cartesian
+    distal_link: contact_3
+    base_link: world
+    indices: [0, 1, 2]
+    
+contact_4:
+    type: Cartesian
+    distal_link: contact_4
+    base_link: world
+    indices: [0, 1, 2]
+
+postural:
+    type: Postural
+    lambda: 0.1
+"""
+
+
+    def __init__(self):
+        # create robot
+        self.cfg = get_xbot_config(prefix='xbotcore/')
+        self.robot = xb.RobotInterface(self.cfg)
+
+        self.joints = sum([[f'hip_roll_{i+1}', f'hip_pitch_{i+1}', f'knee_pitch_{i+1}'] for i in range(4)], [])
+        self.robot.setControlMode(xb.ControlMode.Idle())
+        self.robot.setControlMode({j: xb.ControlMode.Position() for j in self.joints})
+        self.idx = np.array([self.robot.getDofIndex(j) for j in self.joints])
+        self.qidx = self.idx
+
+
+    def move(self, action):
+
+        qname = {
+            'park': 'parked',
+            'unpark': 'home'
+        }[action]
+
+        robot = self.robot
+        joints = self.joints
+        qidx = self.qidx
+
+        while not robot.sense(update_references=True):
+            time.sleep(0.1)
+
+        model = xb.ModelInterface(self.cfg)
+        model.setJointPosition(robot.eigenToMap(robot.getPositionReference()))
+        model.update()
+
+        dt = 0.01
+        ci = pyci.CartesianInterface.MakeInstance(solver='OpenSot', problem=Parking.ikpb, model=model, dt=dt)
+        postural = ci.getTask('Postural')
+
+        t = 0.0
+        trj_time = 5.0
+
+        q0 = model.getJointPosition()[qidx+6]
+        qf = model.getRobotState(qname)[qidx+6]
+
+        while t < trj_time:
+
+            # compute trajectory for postural task
+            tau = t / trj_time
+            alpha = 6 * tau**5 - 15 * tau**4 + 10 * tau**3
+            q = (1-alpha) * q0 + alpha * qf
+
+            # set reference and solve
+            postural.setReferencePosture({joints[i]: q[i] for i in range(len(joints))})
+            ci.update(t, dt)
+            model.setJointPosition(model.getJointPosition() + model.getJointVelocity() * dt)
+            model.update()
+            
+            # set reference to robot and move
+            robot.setPositionReference(model.getJointPositionMap())
+            robot.move()
+            
+            # update time and sync loop
+            t += dt
+            time.sleep(dt)
+
+
+# init ros
+rospy.init_node('parking')
+
+# create parking object
+parking = Parking()
+
+# fall
+if args.z:
+    parking.fall()
+
+# move
+if args.action:
+    parking.move(args.action)
