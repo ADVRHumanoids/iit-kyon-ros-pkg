cf0eee741f75fa2f3cf5166decdd0d64c745ac34
Luca Rossini <luca.rossini@iit.it>
2026-05-19 11:28:03 +0200

    missing xbot2_mujoco

--- docker/noble-ros2/scripts/build.bash
+++ docker/noble-ros2/scripts/build.bash
@@ -27,6 +27,7 @@ forest add-recipes git@github.com:advrhumanoids/multidof_recipes.git -t ros2
 # build
 export PYTHONUNBUFFERED=1
 forest grow iit-kyon-ros-pkg --verbose --clone-depth 1 -j ${FOREST_JOBS:-1} --tag-override hesai_jt128
+forest grow xbot2_mujoco --verbose --clone-depth 1 -j ${FOREST_JOBS:-1}
 
 # build tests
 cd build/iit-kyon-ros-pkg
